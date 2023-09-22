eda9a3a9979b4cba7fe8174f67ff36f10d62b114
HeoJunHyoung <127736102+HeoJunHyoung@users.noreply.github.com>
2023-09-22 13:04:00 +0900

    [level 0] Title: 각도기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.py
+++ 프로그래머스/lv0/120829. 각도기/각도기.py
@@ -1,10 +1,6 @@
 def solution(angle):
     
-    if angle>0 and angle<90:
-        return 1
-    elif angle==90:
-        return 2
-    elif angle>90 and angle<180:
-        return 3
-    else:
-        return 4
+    if angle>0 and angle<=90:
+        return 1 if angle<90 else 2
+    elif angle>90 and angle<=180:
+        return 3 if angle<180 else 4
