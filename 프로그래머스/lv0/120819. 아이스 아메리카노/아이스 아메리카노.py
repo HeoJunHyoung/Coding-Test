6512f3e5b7312a72a0779d954438bb4e8f9ec98d
HeoJunHyoung <127736102+HeoJunHyoung@users.noreply.github.com>
2023-09-22 15:48:02 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.00 ms, Memory: 9.96 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.py
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.py
@@ -1,6 +1,4 @@
 def solution(money):
-    numberOfAmericano = money//5500;
-    lastMoney = money%5500;
     result = []
-    result.extend([numberOfAmericano, lastMoney])
+    result.extend([money//5500, money%5500])
     return result
