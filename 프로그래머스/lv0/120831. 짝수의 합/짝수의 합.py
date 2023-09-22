427297e37e5602fa6015a54b8fa6d2abf0f310c0
HeoJunHyoung <127736102+HeoJunHyoung@users.noreply.github.com>
2023-09-22 13:08:47 +0900

    [level 0] Title: 짝수의 합, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.py
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.py
@@ -1,7 +1,2 @@
 def solution(n):
-    listSum = [i for i in range(0,n+1) if i%2==0] # 0, 1, 2, 3, 4
-    sum = 0
-    for i in listSum:
-        sum += i
-        
-    return sum
+    return sum([i for i in range(0,n+1) if i%2==0]) # 0, 1, 2, 3, 4
