c7146ed41d990fd94666a4b0996e4f4cc5116ecb
HeoJunHyoung <127736102+HeoJunHyoung@users.noreply.github.com>
2023-09-23 16:01:51 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.py
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.py
@@ -1,4 +1,8 @@
 def solution(my_string):
     
-    return my_string[::-1]
+    # return my_string[::-1]
+    
+    my_string = list(my_string)
+    my_string.reverse()
+    return ''.join(my_string)
     
