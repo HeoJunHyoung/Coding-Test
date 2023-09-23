c23881435cabbecb9bfe2003145c3006b379f10f
HeoJunHyoung <127736102+HeoJunHyoung@users.noreply.github.com>
2023-09-23 16:29:32 +0900

    [level 0] Title: 순서쌍의 개수, Time: 57.56 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.py
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.py
@@ -1,6 +1,4 @@
 def solution(n):
-    result = 0
-    for i in range(1, n+1):
-        if n%i == 0:
-            result += 1
-    return result
+    
+    
+    return len([i for i in range(1, n+1) if n%i == 0])
