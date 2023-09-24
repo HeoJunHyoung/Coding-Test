08ce88ecb8ca882e7ab38d786ec3447b6aa504e2
HeoJunHyoung <127736102+HeoJunHyoung@users.noreply.github.com>
2023-09-24 12:56:25 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.py
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.py
@@ -1,12 +1,6 @@
 def solution(rsp):
     
     rsp_dict = dict(zip(['0','2','5'],['5','0','2']))
-    newList = []
-    result = None
     
-    for i in range(0, len(rsp)):
-        newList.append(rsp_dict[rsp[i]])
-        
-    return ''.join(newList)
-    
+    return ''.join([rsp_dict[i] for i in rsp])
     
