094048d38c6ed102da8ecbc311418b117cd544ce
HeoJunHyoung <127736102+HeoJunHyoung@users.noreply.github.com>
2023-09-27 10:41:32 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.17 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.py
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -1,11 +1,4 @@
 def solution(my_string):
     answer = ''
     
-    for i in range(0, len(my_string)):
-        if my_string[i] < 'a':
-            answer += my_string[i].lower()
-        else:
-            answer += my_string[i].upper()
-    
-    
-    return answer
+    return ''.join([my_string[i].lower() if my_string[i] < 'a' else my_string[i].upper() for i in range(0, len(my_string))])
