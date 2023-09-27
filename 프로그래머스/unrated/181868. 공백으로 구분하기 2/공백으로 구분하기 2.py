8f1b85085c9594525b83171450f1661180a4c848
HeoJunHyoung <127736102+HeoJunHyoung@users.noreply.github.com>
2023-09-27 15:59:37 +0900

    [unrated] Title: 공백으로 구분하기 2, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.py
@@ -0,0 +1,10 @@
+def solution(my_string):
+    
+    answer = []
+    
+    my_string = my_string.split(' ')
+    for i in my_string:
+        if i != '':
+            answer.append(i)
+    
+    return answer
