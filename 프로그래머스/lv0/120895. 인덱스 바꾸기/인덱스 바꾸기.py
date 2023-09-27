3d1bcd97581bbf4547971a50f8e7e428689239c3
HeoJunHyoung <127736102+HeoJunHyoung@users.noreply.github.com>
2023-09-27 10:53:31 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
@@ -0,0 +1,13 @@
+def solution(my_string, num1, num2):
+    answer = ''
+    temp = num1
+    
+    for i in range(0, len(my_string)):
+        if i == num1:
+            answer += my_string[num2]
+        elif i == num2:
+            answer += my_string[temp]
+        else:
+            answer += my_string[i]
+    
+    return answer
