5c154537fb09fd55640356ff8b8df0bd5c4d3583
HeoJunHyoung <127736102+HeoJunHyoung@users.noreply.github.com>
2023-10-06 10:07:11 +0900

    [level 1] Title: 예산, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.py
+++ 프로그래머스/lv1/12982. 예산/예산.py
@@ -1,24 +1,20 @@
 def solution(d, budget):
     
-    spent_money = 0
-    result = 0
     d = sorted(d)
+    count = 0
     
-    # 최소금액이 예산보다 큰 경우는 아무것도 살 수 없음
-    if d[0] > budget:
+    if d[0]>budget:
         return 0
     
     for i in d:
-        if spent_money > budget:
-            result -= 1
-            break
-        elif spent_money == budget:
-            break
-        else:
-            spent_money += i
-            result += 1
+        if budget - i >= 0:
+            budget = budget - i
+            count += 1
     
-    return result
+    return count
+    
+    
+        
 
 
 
