a90185ba562ae7990de82a7fa5f56738d3d07f63
HeoJunHyoung <127736102+HeoJunHyoung@users.noreply.github.com>
2023-10-06 12:51:01 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.05 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -0,0 +1,54 @@
+def solution(n, arr1, arr2):
+    
+    result1 = []
+    result2 = []
+    result = []
+    temp = ''
+    
+    for i in range(n):
+        row1 = ''
+        jisoo1 = n-1
+        row2 = ''
+        jisoo2 = n-1
+        for j in range(n):
+            
+            if arr1[i]//(2**jisoo1) == 0:
+                row1 += '0'
+                jisoo1 -=1
+            else:
+                arr1[i] = arr1[i]%(2**jisoo1)
+                row1 += '1'
+                jisoo1 -= 1
+                
+            if arr2[i]//(2**jisoo2) == 0:
+                row2 += '0'
+                jisoo2 -=1
+            else:
+                arr2[i] = arr2[i]%(2**jisoo2)
+                row2 += '1'
+                jisoo2 -= 1
+                
+        result1.append(row1)
+        result2.append(row2)
+        
+    #print(result1)
+    #print(result2)
+    
+    for i in range(n):
+        temp = ''
+        for j in range(n):
+            temp += '#' if int(result1[i][j]) or int(result2[i][j]) else ' '
+            #temp += str(int(result1[i][j]) or int(result2[i][j]))
+        result.append(temp)    
+        
+    #print(result)
+    
+    return result
+    
+    
+    
+    
+    
+    
+    
+    
