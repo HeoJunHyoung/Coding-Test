25918758a45f1dae59987634f86dd2cba3fca1eb
HeoJunHyoung <127736102+HeoJunHyoung@users.noreply.github.com>
2023-10-07 10:42:32 +0900

    [unrated] Title: 콜라 문제, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.py
@@ -2,14 +2,10 @@ def solution(a, b, n):
     
     total = n
     new_cola = 0
-    remain = 0
     
     while total >= a:
-        
-        new_cola = new_cola + total // a * b #실제로 내가 환급 받았던 콜라 수 (정답)
-        print('new cola : {}'.format(new_cola))
+        new_cola = new_cola + total // a * b
         total = (total // a * b) + (total % a)
-        print('total : {}'.format(total))
     
     return new_cola
     
