16f9771d527377e49cecc84769b00053ae00e473
HeoJunHyoung <127736102+HeoJunHyoung@users.noreply.github.com>
2023-10-09 11:00:25 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -4,8 +4,6 @@ def solution(n, words):
     for i in range(len(words)-1):
         stack.append(words[i])
         if words[i][-1] != words[i+1][0] or words[i+1] in stack:
-            #print(words[i+1])
-            print(i+2)
             if (i+2)/n > (i+2)//n:
                 return [(i+1)%n+1, (i+2)//n+1]
             else:  
