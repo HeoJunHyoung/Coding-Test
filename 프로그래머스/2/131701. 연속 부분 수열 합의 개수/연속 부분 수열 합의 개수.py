47bdf0f2ee71c3e043b722e42b8c184e55fc1838
HeoJunHyoung <127736102+HeoJunHyoung@users.noreply.github.com>
2023-10-12 18:39:33 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 4349.47 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,18 +1,13 @@
 def solution(elements):
     
-    origin = elements[:]
     end = len(elements)
     elements *= 2
     sum_list = []
     
-    #print(end)
-    #print(elements)
     
-    
-    for bind in range(1, end): # 1 2 3 4 5
-        for i in range(0, end): # 0 1 2 3 4
+    for bind in range(1, end):
+        for i in range(0, end):
             sum_list.append(sum(elements[i:i+bind]))
     
-    sum_list.append(sum(origin))
     
-    return len(set(sum_list))
+    return len(set(sum_list))+1
