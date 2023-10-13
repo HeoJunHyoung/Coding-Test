dd1f9aac6f0dde58ef1a99621dc00ad75a5f5b0c
HeoJunHyoung <127736102+HeoJunHyoung@users.noreply.github.com>
2023-10-13 11:06:14 +0900

    [level 2] Title: n^2 배열 자르기, Time: 22.09 ms, Memory: 18 MB -BaekjoonHub

---
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
@@ -0,0 +1,38 @@
+def solution(n, left, right):
+    
+    answer = []
+    
+    for i in range(left, right+1):
+        a = i//n
+        b = i%n
+        if a<b:
+            a,b = b,a
+        answer.append(a+1)
+    
+    return answer
+
+
+
+
+
+
+
+
+
+#def solution(n, left, right):
+    
+    #arr = [[0]*n for i in range(n)]
+    #result = []
+    
+    #for i in range(n):
+        #for j in range(0, i+1):
+            #arr[i][j] = i+1
+            #arr[j][i] = i+1
+            
+    
+    #result = sum(arr, [])
+    
+    #return result[left:right+1]
+    
+    
+        
