2c2cf64a2d95c348b77d8adabde80f2c6822c7f1
HeoJunHyoung <127736102+HeoJunHyoung@users.noreply.github.com>
2023-10-13 10:00:53 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+def solution(s):
+    
+    rotated = ''
+    answer = 0
+    
+    if len(s)%2!=0:
+        return 0
+    
+    for i in range(0, len(s)):
+        count = 0
+        rotated = s[i:] + s[0:i]
+        #print(rotated)
+        dq = deque()
+        for j in range(0, len(s)):
+            dq.append(rotated[j])
+            #print(dq)
+            if len(dq)>=2:
+                if dq[-2]+dq[-1]=='[]' or dq[-2]+dq[-1]=='()' or dq[-2]+dq[-1]=='{}':
+                    count += 1
+                    dq.pop()
+                    dq.pop()
+        if count == len(s)//2:
+            answer += 1
+    
+    return answer
+                
+        
