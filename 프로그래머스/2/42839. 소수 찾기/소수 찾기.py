7ebf9b71a78aeb565083b6e8ec25e2d7f4568c72
HeoJunHyoung <127736102+HeoJunHyoung@users.noreply.github.com>
2023-10-30 19:00:44 +0900

    [level 2] Title: 소수 찾기, Time: 0.63 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -0,0 +1,28 @@
+from itertools import combinations
+from itertools import permutations
+
+def isPrime(n):
+    if n<2:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+def solution(numbers):
+    cnt = 0
+    cases = []
+                     
+    for i in range(1, len(numbers)+1):
+        for pick in permutations(numbers, i):
+            cases.append(''.join(pick))
+    
+    cases = list(set([int(i) for i in cases]))
+    
+    
+    for case in cases:
+        if isPrime(int(case)):
+            cnt += 1
+    
+    return cnt
+    
