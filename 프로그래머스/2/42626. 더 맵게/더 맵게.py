d5526fb5381b5e7b61593c73686e581e9bf67898
HeoJunHyoung <127736102+HeoJunHyoung@users.noreply.github.com>
2023-11-28 17:19:48 +0900

    [level 2] Title: 더 맵게, Time: 1607.97 ms, Memory: 67.6 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,19 +1,13 @@
 import heapq
 
 def solution(scoville, K):
-    blend_count = 0
-    blend_scoville = 0
-    heapq.heapify(scoville)
-
-    while scoville[0] < K:
-        first = heapq.heappop(scoville)
-        if not scoville:
-            if first < K:
-                return -1
-        second = heapq.heappop(scoville)
-        blend_scoville = first + (second * 2)
-
-        heapq.heappush(scoville, blend_scoville)
-        blend_count += 1
-
-    return blend_count
+    heap = sorted(scoville)
+    
+    cnt = 0
+    while heap[0] < K:
+        if len(heap) <= 1:
+            break
+        heapq.heappush(heap, heapq.heappop(heap) + heapq.heappop(heap)*2)
+        cnt += 1
+        
+    return cnt if heap[0]>=K else -1
