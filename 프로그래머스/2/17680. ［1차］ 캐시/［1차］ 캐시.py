ca8e6f0f7419a092abbb807a98dc56b2de5b477c
HeoJunHyoung <127736102+HeoJunHyoung@users.noreply.github.com>
2023-11-28 17:45:45 +0900

    [level 2] Title: [1차] 캐시, Time: 0.93 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -1,32 +1,33 @@
+from collections import deque
+
 def solution(cacheSize, cities):
     
-    time = 0
-    memory = []
-    cities = [city.lower() for city in cities]
+    cache = deque()
+    running_time = 0
+    cities = [city.upper() for city in cities]
     
-    if cacheSize==0:
-        return len(cities)*5
-    else:
-        for city in cities:
-            if len(memory) < cacheSize:
-                if city not in memory:
-                    memory.append(city)
-                    time += 5
-
-                else:
-                    recall = memory.pop(memory.index(city))
-                    memory.append(recall)
-                    time += 1
-
+    if cacheSize == 0:
+        return 5 * len(cities)
+    
+    for city in cities:
+        if len(cache)+1 <= cacheSize:
+            if city not in cache:
+                cache.append(city)
+                running_time += 5
             else:
-                if city not in memory:
-                    memory.pop(0)
-                    memory.append(city)
-                    time += 5
-
-                else:
-                    recall = memory.pop(memory.index(city))
-                    memory.append(recall)
-                    time += 1
-                
-    return time
+                delete_index = cache.index(city)
+                cache.append(cache[delete_index])
+                del cache[delete_index]
+                running_time += 1
+        elif len(cache)+1 > cacheSize:
+            if city not in cache:
+                cache.popleft()
+                cache.append(city)
+                running_time += 5
+            else:
+                delete_index = cache.index(city)
+                cache.append(cache[delete_index])
+                del cache[delete_index]
+                running_time += 1
+        
+    return running_time
