0da9bf4aab99c7c4ededdae841fb405c4d064ab0
HeoJunHyoung <127736102+HeoJunHyoung@users.noreply.github.com>
2023-11-30 09:49:30 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.13 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -1,51 +1,29 @@
 def solution(str1, str2):
     
-    # 먼저 str1과 str2은 대,소문자 구분이 없으므로 upper 혹은 lower 수행
-    str1, str2 = str1.upper(), str2.upper()
-    
-    # 각각을 2개씩 묶어야 함.
-    arr1, arr2 = [], []
-    
-    for i in range(len(str1)-1):
-        if str1[i].isalpha() and str1[i+1].isalpha():
-            arr1.append(str1[i]+str1[i+1])
-            
-    for i in range(len(str2)-1):
-        if str2[i].isalpha() and str2[i+1].isalpha():
-            arr2.append(str2[i]+str2[i+1])
-    
-    # 교집합의 개수 계산 : arr1과 arr2의 교집합을 구하고, 교집합의 원소들을 arr1과 arr2에 순회하면서 최소값을
-    #                   중복 교집합으로 설정
-    gyo_count = 0
-    duples = set(arr1) & set(arr2)
-    
-    for duple in duples:
-        gyo_count += min(arr1.count(duple), arr2.count(duple))
-        
-    
-    # 합집합의 개수 계산
-    hap_count = len(arr1)+len(arr2)-gyo_count
-    
-    # 최종결과
-    
-    if gyo_count==0 and hap_count!=0:
-        return 0
-    elif gyo_count==0 and hap_count==0:
+    A = [str1[i:i+2].upper() for i in range(len(str1)-1) if str1[i:i+2].isalpha()]
+    B = [str2[i:i+2].upper() for i in range(len(str2)-1) if str2[i:i+2].isalpha()]
+    print(A)
+    print(B)
+    if not A and not B:
         return 65536
-    else:
-        return int(gyo_count / hap_count * 65536) 
     
     
     
+    setA = list(set(A))
+    setB = list(set(B))
     
+    gyo, hap = 0, 0
     
+    print(setA)
+    print(setB)
     
+    for x in setA:
+        if x in A and x in B:
+            gyo += min(A.count(x), B.count(x))
+    #print(gyo)
     
+    for x in list(set(setA+setB)):
+        hap += max(A.count(x), B.count(x))
+    #print(hap)
     
-    
-    
-    
-    
-    
-    
-    
+    return int(gyo/hap*65536)
