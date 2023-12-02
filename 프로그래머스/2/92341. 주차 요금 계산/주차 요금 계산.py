52e7162ac7b1aa17ba8febb6717311ccceccfb36
HeoJunHyoung <127736102+HeoJunHyoung@users.noreply.github.com>
2023-12-02 23:11:40 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -1,43 +1,38 @@
-import math
-
-def splitInfo(record, parking):
-    time, car_num, state = record.split(' ')
-    hour, minute = time.split(':')
-    minutes = int(hour) * 60 + int(minute)
-    if car_num not in parking:
-        parking[car_num] = ['OUT', 0, 0]
-    return parking, minutes, car_num, state
+def convertHourToMin(time):
+    hours, minutes = list(map(int, time.split(':')))
+    return hours * 60 + minutes
 
 def solution(fees, records):
     
-    parking = dict()
-    result = []
+    car_dict = dict()
+    total_price = []
     
     for record in records:
-        parking, time, car_num, state = splitInfo(record, parking)
-        
-        if parking[car_num][0] == 'IN':
-            parking[car_num][2] += time - parking[car_num][1]
-            parking[car_num][1] = 0
-            parking[car_num][0] = 'OUT'
-            
-        elif parking[car_num][0] == 'OUT':
-            parking[car_num][1] = time
-            parking[car_num][0] = 'IN'
-    
-    for key, value in parking.items():
-        if parking[key][0] == 'IN':
-            parking[key][2] += (23*60+59) - parking[key][1]
-    
-    parking_list = sorted(parking)
-    for car in parking_list:
-        total_money = 0
-        if parking[car][2] > fees[0]:
-            total_money = fees[1] + math.ceil(((parking[car][2] - fees[0]) / fees[2])) * fees[3]
+        time, car_num, state = record.split(' ')
+        time = convertHourToMin(time)
+        if car_num not in car_dict:
+            car_dict[car_num] = [time, 1, 0]
         else:
-            total_money = fees[1]
-        result.append(total_money)
+            if car_dict[car_num][1] == 1:
+                car_dict[car_num] = [0, 0, car_dict[car_num][2] + time - car_dict[car_num][0]]
+            else:
+                car_dict[car_num] = [time, 1, car_dict[car_num][2]]
+        
+    for key, value in car_dict.items():
+        if value[1] == 1:
+            car_dict[key] = [0, 0, value[2] + convertHourToMin("23:59") - car_dict[key][0]]
     
-    return result
+    car_dict = sorted(car_dict.items(), key=lambda x : x[0])
+    
+    
+    for idx, value in enumerate(car_dict):
+        if value[1][2] > fees[0]:
+            if (value[1][2]-fees[0]) % fees[2] == 0:
+                total_price.append(fees[1] + ((value[1][2]-fees[0])/fees[2] * fees[3]))
+            else:
+                total_price.append(fees[1] + (((value[1][2]-fees[0])//fees[2]+1) * fees[3]))
+        else:
+            total_price.append(fees[1])
+    return total_price
     
     
